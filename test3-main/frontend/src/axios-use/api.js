6e76ef6c7000bf6cb11b13cfa4298c2c63682a5e
Aqibgh <aqibali280@gmail.com>
2025-05-29 06:30:53 +0500

    Update frontend and backend integration

--- test3-main/frontend/src/axios-use/api.js
+++ test3-main/frontend/src/axios-use/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configure Axios instance
 const apiClient = axios.create({
-  baseURL: 'http://213.192.2.76:40115/api/',
+  baseURL: 'https://213.192.2.109:40043/api/',
   withCredentials: true, // Required for cookies
   headers: {
     'Content-Type': 'application/json',
